6f5d8c6799452c1cccfc0d181d1352e9fa001675
Servando David Machado Villatoro <servandomachado699@gmail.com>
2025-06-03 01:15:11 -0600

    index.php actualizado

--- index.php
+++ index.php
@@ -4,7 +4,12 @@ require __DIR__ . '/vendor/autoload.php';
 // 2) Carga las variables de .env
 use Dotenv\Dotenv;
 $dotenv = Dotenv::createImmutable(__DIR__);
-$dotenv->load();
+$dotenv->safeLoad();
+
+if (!isset($_ENV['DB_HOST'])) {
+    die('Falta una variable de entorno crítica');
+}
+
 
 // 4) Luego, carga tus rutas/controladores
 require __DIR__ . '/config/route.php';
